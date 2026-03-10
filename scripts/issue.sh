ba38e95ff3ebeae2d88659bf5cfb9ca538849da4
Juan Rozas Santos <juanbr.rozas2005@gmail.com>
2026-03-10 18:39:44 -0300

    issuelogo-config

---
+++ scripts/issue.sh
@@ -0,0 +1,26 @@
+cd /etc
+
+apt-get install issuelogo
+
+ls issue*
+
+cp issue issue.bkp
+
+cat issue.linuxlogo > issue
+
+ls motd*
+
+mv motd motd.bkp
+
+vi motd
+
+-Bem vindo!
+-
+-
+-
+-
+-
+-
+esc
+:wq
+enter
