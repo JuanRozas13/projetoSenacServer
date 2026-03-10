b583edb73096c66f41c8bc39167c5b44b76a716c
Juan Rozas Santos <juanbr.rozas2005@gmail.com>
2026-03-10 18:00:15 -0300

    ip-config

--- scripts/gateway/ativar-ip-forward.sh
+++ scripts/gateway/ativar-ip-forward.sh
@@ -1,1 +1,14 @@
 # Ativar roteamento
+cd etc/network/interface
+
+# A interface loopback (padrão, não mexa)
+auto lo
+iface lo inet loopback
+
+# Sua interface principal (ex: eth0 ou enp0s3)
+auto enp0s3
+iface enp0s3 inet static
+    address 192.168.1.12
+    netmask 255.255.255.192
+    gateway 192.168.1.1
+    dns-nameservers 8.8.8.8 1.1.1.1
