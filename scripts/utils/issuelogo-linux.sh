6dd38f659192b8e702c57b1d6be48497d804acd4
Juan Rozas Santos <juanbr.rozas2005@gmail.com>
2026-03-11 17:58:38 -0300

    mv-linuxlogo/new-ststemD

---
+++ scripts/utils/issuelogo-linux.sh
@@ -0,0 +1,36 @@
+# Instalação do linuxlogo
+cd etc
+
+ls issue*
+
+cat issue
+
+apt-get install linuxlogo
+
+ls issue*
+
+cat issue.linuxlogo
+
+cp issue issue.bkp
+
+ls issue*
+
+cat issue.linuxlogo > issue
+
+ls motd*
+
+cat motd
+
+mv motd motd.bkp
+
+ls motd*
+
+nano motd
+
+-Bem vindo
+-
+-
+-
+-
+esc
+shift :qw
